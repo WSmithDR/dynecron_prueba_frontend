3711f6739534073a2cead1eeb16303edfffc88c2
WSmithDR <wsduenas@espol.edu.ec>
2025-08-22 12:12:32 -0500

    aparetnemente todos lo compoentnes se someten al mdo oscuro

--- src/pages/UploadPage/UploadPage.tsx
+++
@@ -1,4 +0,0 @@
-// This is a temporary file to help with the transition to the new path
-// Please update your imports to use: import UploadPage from './pages/UploadPage';
-
-export { default } from './index';
